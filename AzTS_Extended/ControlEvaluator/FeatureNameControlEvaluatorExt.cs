c9a33eb86ffb389d95fc4804165af020401f2b99
Freya Mehta <freyamehta@microsoft.com>
2021-09-27 10:31:59 +0530

    Modifying Extension CE

--- AzTS_Extended/ControlEvaluator/FeatureNameControlEvaluatorExt.cs
+++ AzTS_Extended/ControlEvaluator/FeatureNameControlEvaluatorExt.cs
@@ -10,7 +10,7 @@
     using System.Linq;
     using System.Text;
 
-    class FeatureNameEvaluatorExt : FeatureNameEvaluator
+    class FeatureNameEvaluatorExt : BaseControlEvaluator
     {
  
     }
